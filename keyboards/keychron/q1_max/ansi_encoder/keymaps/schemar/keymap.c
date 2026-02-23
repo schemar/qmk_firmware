d0c4066ca2e6dbfce9ca0a64a65139d2b1716b8b
Martin Schenck <martinschenck@fastmail.com>
2026-02-23 10:42:08 +0100

    mac: move modifier one below home row

--- keyboards/keychron/q1_max/ansi_encoder/keymaps/schemar/keymap.c
+++ keyboards/keychron/q1_max/ansi_encoder/keymaps/schemar/keymap.c
@@ -63,25 +63,25 @@ tap_dance_action_t tap_dance_actions[] = {
 #define MOD_ESC LT(MOD1, KC_ESC) // Mod layer when held, ESC when tapped.
 // Home Row
 // Left hand
-#define HOME_A MT(MOD_LCTL, KC_A)
-#define HOME_S MT(MOD_LALT, KC_S)
-#define HOME_D MT(MOD_LSFT, KC_D)
-#define HOME_F MT(MOD_LGUI, KC_F)
+#define HOME_ZC MT(MOD_LCTL, KC_Z)
+#define HOME_X MT(MOD_LALT, KC_X)
+#define HOME_C MT(MOD_LSFT, KC_C)
+#define HOME_VG MT(MOD_LGUI, KC_V)
 // Right hand
-#define HOME_J MT(MOD_RGUI, KC_J)
-#define HOME_K MT(MOD_RSFT, KC_K)
-#define HOME_L MT(MOD_LALT, KC_L) // Using LALT for better compatibility.
-#define HOME_SCLN MT(MOD_RCTL, KC_SCLN)
+#define HOME_MC MT(MOD_RCTL, KC_M)
+#define HOME_COMM MT(MOD_RSFT, KC_COMM)
+#define HOME_DOT MT(MOD_LALT, KC_DOT) // Using LALT for better compatibility.
+#define HOME_SLHG MT(MOD_RGUI, KC_SLSH)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [MAC_BASE] = LAYOUT_ansi_82(
-        KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,        KC_VOLD,  KC_VOLU,  KC_DEL,             KC_MUTE,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,           KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,           KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        MOD_ESC,  HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,      LT(MOD1, KC_QUOT),  KC_ENT,             KC_HOME,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,         KC_SLSH,            KC_RSFT,  KC_UP,
-TD(TD_CTL_HYP),   KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD,MO(MAC_FN),KC_RCTL,        KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE, KC_VOLD,   KC_VOLU,  KC_DEL,             KC_MUTE,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    KC_MINS,   KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,    KC_LBRC,   KC_RBRC,  KC_BSLS,            KC_PGDN,
+        MOD_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN, KC_QUOT,   KC_ENT,                       KC_HOME,
+        KC_LSFT,            HOME_ZC,  HOME_X,   HOME_C,   HOME_VG,  KC_B,     KC_N,     HOME_MG,  HOME_COMM,HOME_DOT,HOME_SLHC, KC_RSFT,            KC_UP,
+TD(TD_CTL_HYP),   KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD,MO(MAC_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_ansi_82(
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,            RGB_TOG,
