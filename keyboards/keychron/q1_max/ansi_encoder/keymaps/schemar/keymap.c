ab420b62c7a049dfa0a7e865820dfbdbb460d21d
Martin Schenck <martinschenck@fastmail.com>
2026-02-23 10:42:32 +0100

    win: add modifiers to row below home row

--- keyboards/keychron/q1_max/ansi_encoder/keymaps/schemar/keymap.c
+++ keyboards/keychron/q1_max/ansi_encoder/keymaps/schemar/keymap.c
@@ -64,13 +64,17 @@ tap_dance_action_t tap_dance_actions[] = {
 // Home Row
 // Left hand
 #define HOME_ZC MT(MOD_LCTL, KC_Z)
+#define HOME_ZG MT(MOD_LGUI, KC_Z)
 #define HOME_X MT(MOD_LALT, KC_X)
 #define HOME_C MT(MOD_LSFT, KC_C)
+#define HOME_VC MT(MOD_LCTL, KC_V)
 #define HOME_VG MT(MOD_LGUI, KC_V)
 // Right hand
 #define HOME_MC MT(MOD_RCTL, KC_M)
+#define HOME_MG MT(MOD_RGUI, KC_M)
 #define HOME_COMM MT(MOD_RSFT, KC_COMM)
 #define HOME_DOT MT(MOD_LALT, KC_DOT) // Using LALT for better compatibility.
+#define HOME_SLHC MT(MOD_RCTL, KC_SLSH)
 #define HOME_SLHG MT(MOD_RGUI, KC_SLSH)
 
 // clang-format off
@@ -92,11 +96,11 @@ TD(TD_CTL_HYP),   KC_LOPTN, KC_LCMMD,                               KC_SPC,     
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_ansi_82(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,             KC_MUTE,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,    KC_F12,   KC_DEL,             KC_MUTE,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    KC_MINS,   KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,    KC_LBRC,   KC_RBRC,  KC_BSLS,            KC_PGDN,
+        MOD_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN, KC_QUOT,   KC_ENT,                       KC_HOME,
+        KC_LSFT,            HOME_ZG,  HOME_X,   HOME_C,   HOME_VC,  KC_B,     KC_N,     HOME_MC,  HOME_COMM,HOME_DOT,HOME_SLHG, KC_RSFT,            KC_UP,
         KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_ansi_82(
