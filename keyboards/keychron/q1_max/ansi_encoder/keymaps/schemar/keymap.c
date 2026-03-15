71748bf30982a30e48dc139545381fed87ba4413
Martin Schenck <martinschenck@fastmail.com>
2026-03-15 16:04:58 +0100

    formatting

--- keyboards/keychron/q1_max/ansi_encoder/keymaps/schemar/keymap.c
+++ keyboards/keychron/q1_max/ansi_encoder/keymaps/schemar/keymap.c
@@ -79,12 +79,12 @@ tap_dance_action_t tap_dance_actions[] = {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
- [MAC_BASE] = LAYOUT_ansi_82(
+    [MAC_BASE] = LAYOUT_ansi_82(
         KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE, KC_VOLD,   KC_VOLU,  KC_DEL,             KC_MUTE,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    KC_MINS,   KC_EQL,   KC_BSPC,            KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,    KC_LBRC,   KC_RBRC,  KC_BSLS,            KC_PGDN,
         MOD_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN, KC_QUOT,   KC_ENT,                       KC_HOME,
-        KC_LSFT,            MOD_ZC,   MOD_X,    MOD_C,    MOD_VG,  KC_B,     KC_N,      MOD_MG,   MOD_COMM, MOD_DOT, MOD_SLHC, KC_RSFT,            KC_UP,
+        KC_LSFT,            MOD_ZC,   MOD_X,    MOD_C,    MOD_VG,   KC_B,     KC_N,     MOD_MG,   MOD_COMM, MOD_DOT, MOD_SLHC,  KC_RSFT,            KC_UP,
 TD(TD_CTL_HYP),   KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD,MO(MAC_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_ansi_82(
@@ -100,7 +100,7 @@ TD(TD_CTL_HYP),   KC_LOPTN, KC_LCMMD,                               KC_SPC,     
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    KC_MINS,   KC_EQL,   KC_BSPC,            KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,    KC_LBRC,   KC_RBRC,  KC_BSLS,            KC_PGDN,
         MOD_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN, KC_QUOT,   KC_ENT,                       KC_HOME,
-        KC_LSFT,            MOD_ZG,   MOD_X,    MOD_C,    MOD_VC,  KC_B,     KC_N,      MOD_MC,   MOD_COMM, MOD_DOT, MOD_SLHG, KC_RSFT,            KC_UP,
+        KC_LSFT,            MOD_ZG,   MOD_X,    MOD_C,    MOD_VC,   KC_B,     KC_N,     MOD_MC,   MOD_COMM, MOD_DOT, MOD_SLHG,  KC_RSFT,            KC_UP,
         KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_ansi_82(
